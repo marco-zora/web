2ecc02415b8ce2d55a893d94eb3876ba387d94b0
marco-zora <marcomaria.zora@cnr.it>
2026-02-02 17:41:36 +0100

    login

--- obj/Debug/net10.0/BlazorApp1.AssemblyInfo.cs
+++ obj/Debug/net10.0/BlazorApp1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6890bdcd6a5f19be1e6448aa158b7b5ef4ed704a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+aa3b3b3b98277a70971a7e0435aea92ead36b595")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- obj/Debug/net10.0/dotnet.js
+++ obj/Debug/net10.0/dotnet.js
@@ -5,7 +5,7 @@ var e=!1;const t=async()=>WebAssembly.validate(new Uint8Array([0,97,115,109,1,0,
   "mainAssemblyName": "BlazorApp1",
   "applicationEnvironment": "Development",
   "resources": {
-    "hash": "sha256-l29yYp5L7G/VY8gELQGkUG7PMAUuQZweSfHKuTHTlkw=",
+    "hash": "sha256-CnmmE2Iihwkg7Q60DYMhH+HPOVJFSgD55kSWysBWTGk=",
     "jsModuleNative": [
       {
         "name": "dotnet.native.87vtjjdetb.js"
@@ -1242,16 +1242,16 @@ var e=!1;const t=async()=>WebAssembly.validate(new Uint8Array([0,97,115,109,1,0,
       },
       {
         "virtualPath": "BlazorApp1.wasm",
-        "name": "BlazorApp1.3ohksrnjoj.wasm",
-        "integrity": "sha256-jVFXmmVf87f7nqpD2F7MrGprVBYs+4HGdfP0uWsO5zc=",
+        "name": "BlazorApp1.5aemen184q.wasm",
+        "integrity": "sha256-zYv4szw5lg5d2IQ9vatr5vpVQNwlAlY/eeJ1lDI0Jdg=",
         "cache": "force-cache"
       }
     ],
     "pdb": [
       {
         "virtualPath": "BlazorApp1.pdb",
-        "name": "BlazorApp1.8bqv1bfpp4.pdb",
-        "integrity": "sha256-9HPEU6sfX2lgK5Es6ytS1M55bpaAQV8m75mOsqSzSpw=",
+        "name": "BlazorApp1.xawzi3njtp.pdb",
+        "integrity": "sha256-SfywLDIUFc9yNpCCh34cgPCzG2toaEauE9epUdm07cE=",
         "cache": "force-cache"
       }
     ],
